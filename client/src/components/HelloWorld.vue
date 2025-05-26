51c9f05ba6443c0671a5e981ab8de593c9234ebb
oikazeHiro <15093959810@163.com>
2025-05-26 14:50:57 +0800

    test

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import { toggleDark } from '~/composables'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="toggleDark()">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
