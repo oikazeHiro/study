4244b817b8fecdc7bcd6b4da008ae19fea56d366
Oikaze <2889295938@qq.com>
2025-07-16 22:29:00 +0800

    第一个实例

---
+++ threejs/html/index.js
@@ -0,0 +1,5 @@
+import * as THREE from 'three';
+console.log(THREE.Scene);
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+console.log('OrbitControls', OrbitControls)
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
