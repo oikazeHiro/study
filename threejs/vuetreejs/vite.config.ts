7e7ec823d1645553b99af01eb61bbcef7101fa51
oikazeHiro <15093959810@163.com>
2025-07-17 08:50:09 +0800

    Fix: treat threejs/vuetreejs as normal directory

---
+++ threejs/vuetreejs/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path' 
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
+    },
+    plugins: [vue()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    }
+  }
+})
