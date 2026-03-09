4e7176726ab02f5460107bb429d90c5a782ab73a
oikazeHiro <15093959810@163.com>
2026-03-09 16:29:21 +0800

    测试

--- ai_code_test/src/main/java/com/gan/code/ai/AiCodeHelper.java
+++ ai_code_test/src/main/java/com/gan/code/ai/AiCodeHelper.java
@@ -1,6 +1,11 @@
 package com.gan.code.ai;
 
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,4 +16,10 @@ public class AiCodeHelper {
 
     @Resource
     private ChatModel chatModel;
+
+    public String chat(UserMessage message){
+        ChatResponse chatResponse = chatModel.chat(message);
+        AiMessage aiMessage = chatResponse.aiMessage();
+        return aiMessage.text();
+    }
 }
--- ai_code_test/src/test/java/com/gan/code/AiCodeTestApplicationTests.java
+++ ai_code_test/src/test/java/com/gan/code/AiCodeTestApplicationTests.java
@@ -1,13 +1,30 @@
 package com.gan.code;
 
+import com.gan.code.ai.AiCodeHelper;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AiCodeTestApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
+    @Resource
+    private AiCodeHelper aiCodeHelper;
+
+    @Test
+    void imageTest(){
+        UserMessage userMessage = UserMessage.from(
+                TextContent.from("解析图片文字"),
+                ImageContent.from("https://download.ydstatic.com/ead/zhiyun/guanwang_cdn_2019/images/p-ocr/cookbook100K.jpg")
+        );
+        String result = aiCodeHelper.chat(userMessage);
+        System.out.println(result);
+        assertNotNull( result,"");
+    }
 }
